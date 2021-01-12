fd45a8ff96b6d99ccacf616ba99f0bf463d574fc
maru-torres <77306056+maru-torres@users.noreply.github.com>
2021-01-11 17:29:07 -0800

    Adding LogFinderConfig.json and SearchConfig.cs
    LogFinderConfig.json, sample json file that contains the json config that Log Search Tool will use.
    SearchConfig.cs, contains the RootObject and LogSearchConfig classes that will be used for Json deserialization and validation.

---
+++ SearchConfig.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+
+//using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+//using System.Text.Json.Serialization;
+
+public class Rootobject
+{
+    public LogSearchConfig LogSearchConfig { get; set; }
+
+    public override string ToString()
+    {
+        return string.Format(@"Searching for '{0}' in  {2}", LogSearchConfig.StringToSearch, Environment.NewLine, LogSearchConfig.LogPath);
+    }
+}
+
+public class LogSearchConfig
+{
+   // [JsonProperty(Required = Required.Always)]
+   [JsonInclude]
+    public string LogPath { get; set; }
+
+    //[JsonProperty(Required = Required.Always)]
+    [JsonInclude]
+    public string TextFileExtenstion { get; set; }
+
+    //[JsonProperty(Required = Required.DisallowNull)]
+    public string StringToSearch { get; set; }
+
+    public object FileContents { get; private set; }
+}
